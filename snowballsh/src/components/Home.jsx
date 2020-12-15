36909bd5e195a93b6a497d1dff73b08c73c103f8
SnowballSH <derek070211@gmail.com>
2020-12-15 10:20:11 -0600

    projects

--- snowballsh/src/components/Home.jsx
+++ snowballsh/src/components/Home.jsx
@@ -5,10 +5,12 @@ export default function Homepage() {
   return (
     <div className="App-section">
       <div>
+        <br/>
         <img src="images/SnowballSH.png" className="App-logo" alt="SnowballSH"/>
         <p>
           Hi there, this is SnowballSH!
         </p>
+
         <Button
           variant="contained"
           color="primary"
@@ -19,7 +21,32 @@ export default function Homepage() {
         >
           View my projects on Github!
         </Button>
+
+        <br/>
+        <p>
+          These are some of the web applications I made:
+        </p>
+
+        <a
+          target="_blank"
+          href="http://snowballsh.me/JumpingGamePhaser/"
+          rel="noopener noreferrer"
+        >
+          <div className="wrapper">
+            <img src="https://i.imgur.com/Y9BunBO.png" alt="Jumping Game" className="project"/>
+          </div>
+        </a>
+
+        <a
+          target="_blank"
+          href="https://repl.it/@SnowballSH/SimplePokemonGameCLI"
+          rel="noopener noreferrer"
+        >
+          <div className="wrapper">
+            <img src="https://i.imgur.com/ZuzPNTv.png" alt="Pokemon Game" className="project"/>
+          </div>
+        </a>
       </div>
     </div>
-  )
+  );
 }
