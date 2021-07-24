256d45590a25fef59406bdfa3f4cf7c71255438c
SnowballSH <derek070211@gmail.com>
2021-07-23 23:16:47 -0500

    fix layout

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite';
 
 import prefresh from '@prefresh/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-  },
-  build: {
-    outDir: "../docs/",
-  },
-  plugins: [prefresh()]
-})
+    esbuild: {
+        jsxFactory: 'h',
+        jsxFragment: 'Fragment',
+    },
+    build: {
+        outDir: "../docs/",
+        emptyOutDir: true,
+    },
+    plugins: [prefresh()]
+});
