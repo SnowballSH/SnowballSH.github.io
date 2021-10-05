0afe63a36546bb9df205645f84f7ae1b42eee097
SnowballSH <66022611+SnowballSH@users.noreply.github.com>
2021-10-04 21:29:39 -0500

    update ELO

--- web/src/components/projects.tsx
+++ web/src/components/projects.tsx
@@ -1,6 +1,6 @@
 import "../styles/waves.scss";
 import "../styles/grid.scss";
-import {Fragment, h} from "preact";
+import { Fragment, h } from "preact";
 
 import FigurifyPNG from "../figurify.png";
 
@@ -10,18 +10,18 @@ function get_languages(mobile: boolean) {
             <h2>
                 The Gorilla Programming Language
             </h2>
-            <img src={"https://i.imgur.com/lX7Vzr0.png"} alt={""}/>
+            <img src={"https://i.imgur.com/lX7Vzr0.png"} alt={""} />
             <h4>
                 A <b>dynamic, interpreted programming language</b> focusing on simplicity and speed.
-                <br/>
+                <br />
                 Although <a href={"https://github.com/SnowballSH/Gorilla#news"} target={"_blank"} rel="noopener">archived
-                and
-                unfinished</a>,
+                    and
+                    unfinished</a>,
                 Gorilla is my greatest project.
             </h4>
             <h4>
                 From learning to make a parser, to interpreters, to compilations, to VM,
-                <br/>
+                <br />
                 I learned a lot from
                 this project, and it is better than I expected! It is also very cool!
             </h4>
@@ -33,16 +33,16 @@ function get_languages(mobile: boolean) {
             {
                 mobile ? "" : <div class="wave1">
                     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
-                         preserveAspectRatio="none">
+                        preserveAspectRatio="none">
                         <path
                             d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
-                            opacity=".25" class="shape-fill"/>
+                            opacity=".25" class="shape-fill" />
                         <path
                             d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
-                            opacity=".5" class="shape-fill"/>
+                            opacity=".5" class="shape-fill" />
                         <path
                             d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
-                            class="shape-fill"/>
+                            class="shape-fill" />
                     </svg>
                 </div>
             }
@@ -81,13 +81,13 @@ export function Projects() {
                     <h2>
                         Figurify the algorithms, right in your browser!
                     </h2>
-                    <img src={FigurifyPNG} alt={""}/>
+                    <img src={FigurifyPNG} alt={""} />
 
                     <h4>Toolbox website for visualizing everything.</h4>
 
                     <h4><a href={"https://github.com/SnowballSH/Figurify"} target={"_blank"} rel="noopener">Github</a>
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
                         <a href={"https://figurify.vercel.app"} target={"_blank"} rel="noopener">Website</a></h4>
                 </section>
 
@@ -96,15 +96,10 @@ export function Projects() {
                         The Iceburn Chess Engine
                     </h2>
                     <h4>
-                        Iceburn is a simple chess engine written in ~800 lines of Rust code. It does not have much
-                        search pruning
-                        technique, but with the <b>NNUE evaluation and MTD(f) search</b>, it plays at ~500knps with an
-                        ELO
-                        of <a
-                        href={"https://lichess.org/@/IceBurnEngine"} target={"_blank"} rel="noopener">~1600 Blitz and
-                        ~1700 Rapid</a>.
-                        This project is
-                        for fun and is not a focus on engine performance :)
+                        Iceburn is a simple chess engine written Rust. It uses negamax with various pruning and move ordering techniques. It also uses MTD(f) iterative deepening search.
+                        <br />
+                        Development of v2 currently plays at <a
+                            href={"https://lichess.org/@/IceBurnEngine"} target={"_blank"} rel="noopener">1800 bullet and 1700 Blitz</a> on lichess.
                     </h4>
                     <h4><a href={"https://github.com/SnowballSH/iceburn"} target={"_blank"} rel="noopener">Github</a>
                     </h4>
@@ -116,10 +111,10 @@ export function Projects() {
                     </h2>
                     <h4>
                         Rustfish is the Rust port of the Stockfish Chess Engine (which is written in C++).
-                        <br/>
+                        <br />
                         The project is continued from <a href={"https://github.com/syzygy1/Rustfish"} target={"_blank"}
-                                                         rel="noopener">Sygyzy's
-                        Rustfish</a>, but since that repo is in silence for a while, I decided to update it toward
+                            rel="noopener">Sygyzy's
+                            Rustfish</a>, but since that repo is in silence for a while, I decided to update it toward
                         Stockfish 14.
                     </h4>
                     <h4>
@@ -136,7 +131,7 @@ export function Projects() {
                     </h2>
                     <img
                         src={"https://camo.githubusercontent.com/a198ac493fd6e5ad8bc9036bfdebeba25ba449cc82fba2ceaeae1564307135bd/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3739343336313235343838313532353831332f3830353135333937363834373639353932322f69636f6e2e706e67"}
-                        alt={""}/>
+                        alt={""} />
                     <h4>
                         Probox is a online toolbox providing <b>various features</b> for programmers,
                         including Code Compiler, Code Shortener, Stack Overflow searcher, Regex Templates, Ascii Table,
@@ -145,9 +140,9 @@ export function Projects() {
                     <h4>I know most of the tools sound stupid, they are, but this is my first successful project <b>collaborating
                         with other guys</b>.</h4>
                     <h4><a href={"https://github.com/TWT-Code-Jam-FAST/Probox"} target={"_blank"}
-                           rel="noopener">Github</a>
-                        <br/>
-                        <br/>
+                        rel="noopener">Github</a>
+                        <br />
+                        <br />
                         <a href={"https://probox.vercel.app"} target={"_blank"} rel="noopener">Website</a></h4>
                 </section>
 
@@ -165,9 +160,9 @@ export function Projects() {
                         different
                         search times.</h4>
                     <h4><a href={"https://github.com/SnowballSH/minimal-chess"} target={"_blank"}
-                           rel="noopener">Github</a>
-                        <br/>
-                        <br/>
+                        rel="noopener">Github</a>
+                        <br />
+                        <br />
                         <a href={"https://snowballsh.me/minimal-chess"} target={"_blank"} rel="noopener">Website</a>
                     </h4>
                 </section>
